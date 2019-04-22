967161fe083cb32969256b448b1819e393fb6a91
Sohil Ladhani <supertakumi86@gmail.com>
2019-04-23 01:15:59 +0530

    Rustfmt'd the files

--- chapter_03/exercises/fibonacci/src/main.rs
+++ chapter_03/exercises/fibonacci/src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
 
-fn nth_fibonacci(n:i32) -> i32 {
+fn nth_fibonacci(n: i32) -> i32 {
     let mut fibonacci_sum = 1;
-    if n <= 2 { 
-        return n-1;
+    if n <= 2 {
+        return n - 1;
     } else {
-        for element in 2..n-2 {
+        for element in 2..n - 2 {
             fibonacci_sum += element;
         }
     }
@@ -18,10 +18,9 @@ fn main() {
 
         let mut n = String::new();
 
-        io::stdin().read_line(&mut n)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut n).expect("Failed to read line");
 
-        let n:i32 = match n.trim().parse() {
+        let n: i32 = match n.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -30,13 +29,14 @@ fn main() {
 
         println!("Keep going? (y/n)[n]:");
         let mut ans = String::new();
-        io::stdin().read_line(&mut ans)
+        io::stdin()
+            .read_line(&mut ans)
             .expect("Failed to read line");
 
         match ans.as_str().trim() {
             "y" | "Y" => {
                 continue;
-            },
+            }
             _ => {
                 break;
             }
